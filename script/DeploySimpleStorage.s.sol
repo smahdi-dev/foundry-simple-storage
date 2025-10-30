fd88fb2718af8910355313c4f381ff49988dad2e
smahdi-dev <seyedmahdi.hosseini24@gmail.com>
2025-10-30 14:40:59 +0330

    feat: add deployment script for SimpleStorage contract

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+
+        return simpleStorage;
+    }
+}
